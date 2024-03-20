65616c5a2c5cb34c6f910a30cbdcab556fc9b798
Gianluca Klimmer <pixelmannvip@gmail.com>
2024-03-20 09:01:39 +0100

    Add flatzinc parser with queens

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
+use flatzinc::{statement, *};
+use std::{io::Error, process};
+
 fn main() {
-    println!("Hello, world!");
+    if let Err(err) = run() {
+        eprintln!("Error: {err}");
+        process::exit(1);
+    }
+}
+fn run() -> Result<(), Error> {
+    let opt = "problems/queens/queens.fzn";
+    let buf = std::fs::read_to_string(opt)?;
+    for line in buf.lines() {
+        match statement::<VerboseError<&str>>(line) {
+            Ok((_, result)) => println!("{:#?}", result),
+            Err(Err::Error(e)) => {
+                let bla = convert_error(buf.as_str(), e);
+                eprintln!("Failed to parse flatzinc!\n{}", bla)
+            }
+            Err(e) => eprintln!("Failed to parse flatzinc: {:?}", e),
+        }
+    }
+    Ok(())
 }
