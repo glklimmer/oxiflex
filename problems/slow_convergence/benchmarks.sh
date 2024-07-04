97e0766a21305ee765fae73a0e0a9331a68c4db3
Gianluca Klimmer <pixelmannvip@gmail.com>
2024-07-05 01:14:14 +0200

    Add slow convergence benchmark

--- problems/slow_convergence/benchmarks.sh
+++ problems/slow_convergence/benchmarks.sh
@@ -4,17 +4,11 @@
 PROGRAM_PATH='./target/release/oxiflex'
 
 # Define the problem file pattern
-PROBLEM_FILE='problems/slow_convergence/0{n}.fzn'
+PROBLEM_FILE='problems/slow_convergence/{n}.fzn'
 
 hyperfine --warmup 3 \
-  "$PROGRAM_PATH $PROBLEM_FILE -n -r" \
   "$PROGRAM_PATH $PROBLEM_FILE -n" \
-  "$PROGRAM_PATH $PROBLEM_FILE -f -r" \
   "$PROGRAM_PATH $PROBLEM_FILE -f" \
-  "$PROGRAM_PATH $PROBLEM_FILE -a 1 -r" \
-  "$PROGRAM_PATH $PROBLEM_FILE -a 1" \
-  "$PROGRAM_PATH $PROBLEM_FILE -r" \
-  "$PROGRAM_PATH $PROBLEM_FILE" \
-  --parameter-scan n 100 600 \
-  --parameter-step-size 100 \
+  --parameter-scan n 10 60 \
+  --parameter-step-size 10 \
   --export-json problems/slow_convergence/slow_convergence.json
--- problems/slow_convergence/setup.sh
+++ problems/slow_convergence/setup.sh
@@ -2,17 +2,18 @@
 
 # List of numbers
 numbers=(100 200 300 400 500 600)
+numbers=(10 20 30 40 50 60 70 80 90 10)
 
 # Loop through the list
 for num in "${numbers[@]}"; do
   # Format the filename without leading zeros
-  filename="0${num}.dzn"
+  filename="${num}.dzn"
 
   # Create the file with the specified content
   echo -e "\nn = $num;" >problems/slow_convergence/"$filename"
 
   # Format the output filename for the minizinc command
-  outputfile="0${num}.fzn"
+  outputfile="${num}.fzn"
 
   # Execute the minizinc command
   minizinc -c problems/slow_convergence/slow_convergence.mzn -d "problems/slow_convergence/$filename" -o "problems/slow_convergence/$outputfile"
