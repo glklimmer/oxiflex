1bc6d0ce72cd5c412bd288fa462f4daf92e59b40
Gianluca Klimmer <pixelmannvip@gmail.com>
2024-07-06 17:40:19 +0200

    Plot cleanup

--- problems/queens/scripts/plot_iterations.py
+++ problems/queens/scripts/plot_iterations.py
@@ -2,7 +2,7 @@ import json
 import matplotlib.pyplot as plt
 
 # Load the JSON data from the specified file path
-with open('problems/queens/data/count_logging.json', 'r') as file:
+with open('problems/queens/data/iterations.json', 'r') as file:
     data = json.load(file)
 
 # Prepare plot data
@@ -18,15 +18,14 @@ for n in n_values:
 
 # Generate the plot
 n_values_int = list(map(int, n_values))  # Convert n values from string to integers
-plt.figure(figsize=(12, 8))
 for opt, results in plot_data.items():
     label = opt if opt else "no flags"  # Adjusting label for "no flags" option
     plt.plot(n_values_int, results, label=label.replace("_", " ").strip())
 
 plt.title("Number of Iterations (Averaged over 5 Runs)")
 plt.xlabel("n")
-plt.ylabel("Average Result")
+plt.ylabel("Iterations")
 plt.xticks(n_values_int)  # Set x-axis ticks to the correct problem sizes
 plt.legend(title="Options", loc='upper left')
 plt.grid(True)
-plt.savefig("problems/queens/plots/count_logging.png")
+plt.savefig("problems/queens/plots/iterations.png")
